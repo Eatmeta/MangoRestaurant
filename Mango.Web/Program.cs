f721ebf22276334f5b1938625fcc8ba9c8bc701a
Eatmeta <metagroul@gmail.com>
2023-02-13 19:48:18 +0300

    Add and configure Product Service to frontend project

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -1,11 +1,17 @@
+using Mango.Web.Services;
+using Mango.Web.Services.IServices;
+using static Mango.Web.SD;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddHttpClient<IProductService, ProductService>();
+ProductAPIBase = builder.Configuration["ServiceUrls:ProductAPI"];
+builder.Services.AddScoped<IProductService, ProductService>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
