ab825ceda967a130cbcc0df6f2d89b75c4bad987
Eatmeta <metagroul@gmail.com>
2023-02-13 16:52:14 +0300

    Add ProductApi controller

---
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,86 @@
+﻿using Mango.Services.ProductAPI.Models.Dto;
+using Mango.Services.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ProductAPI.Controllers;
+
+[Route("api/products")]
+public class ProductAPIController : ControllerBase
+{
+    protected ResponseDto _response;
+    private IProductRepository _productRepository;
+
+    public ProductAPIController(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+        _response = new ResponseDto();
+    }
+
+    [HttpGet]
+    public async Task<object> Get()
+    {
+        try
+        {
+            var productDtos = await _productRepository.GetProducts();
+            _response.Result = productDtos;
+        }
+        catch(Exception ex)
+        {
+            _response.IsSuccess = false;
+            _response.ErrorMessages = new List<string> { ex.ToString() };
+        }
+        return _response;
+    }
+    
+    [HttpGet]
+    [Route("{id:int}")]
+    public async Task<object> Get(int id)
+    {
+        try
+        {
+            var productDto = await _productRepository.GetProductById(id);
+            _response.Result = productDto;
+        }
+        catch (Exception ex)
+        {
+            _response.IsSuccess = false;
+            _response.ErrorMessages = new List<string> { ex.ToString() };
+        }
+        return _response;
+    }
+    
+    [HttpPost]
+    //[Authorize]
+    public async Task<object> Post([FromBody] ProductDto productDto)
+    {
+        try
+        {
+            var model = await _productRepository.CreateUpdateProduct(productDto);
+            _response.Result = model;
+        }
+        catch (Exception ex)
+        {
+            _response.IsSuccess = false;
+            _response.ErrorMessages = new List<string> { ex.ToString() };
+        }
+        return _response;
+    }
+    
+    [HttpDelete]
+    //[Authorize(Roles ="Admin")]
+    [Route("{id:int}")]
+    public async Task<object> Delete(int id)
+    {
+        try
+        {
+            var isSuccess = await _productRepository.DeleteProduct(id);
+            _response.Result = isSuccess;
+        }
+        catch (Exception ex)
+        {
+            _response.IsSuccess = false;
+            _response.ErrorMessages = new List<string> { ex.ToString() };
+        }
+        return _response;
+    }
+}
--- Mango.Services.ProductAPI/Program.cs
+++ Mango.Services.ProductAPI/Program.cs
@@ -1,6 +1,6 @@
-using AutoMapper;
 using Mango.Services.ProductAPI;
 using Mango.Services.ProductAPI.DbContexts;
+using Mango.Services.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 var mapper = MappingConfig.RegisterMaps().CreateMapper();
 builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddControllers();
 
